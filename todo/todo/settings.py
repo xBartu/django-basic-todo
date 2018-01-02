d11a2af6fe48c6ceb8fd2ce9d5aae530d1d8e583
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-01-02 23:33:56 +0200

    static

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -117,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = '/home/xroot/Desktop/stufff/Courses/Rapid/project/prj/src/todo/static/'
 STATIC_URL = '/static/'

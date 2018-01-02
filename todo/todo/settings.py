659d05bd48ed3a1b0e29e18c149eaf8d21248eb5
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-01-03 01:06:34 +0200

    requirements.txt

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/xroot/Desktop/stufff/Courses/Rapid/project/prj/src/todo/db.sqlite3'),
     }
 }
 

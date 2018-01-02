31ba65c1195b1d301a4d0c8616a31b3a3e23e0a3
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2018-01-02 17:13:46 +0200

    admin panel + model

--- todo/todos/admin.py
+++ todo/todos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from todos.models import ToDos, List
 
-# Register your models here.
+class ToDosAdmin(admin.ModelAdmin):
+    list_display = ('title', 'list', 'due_to')
+    ordering = ('due_to')
+
+admin.site.register(List)
+admin.site.register(Todos, ToDosAdmin)
